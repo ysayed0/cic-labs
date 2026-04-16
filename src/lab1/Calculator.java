8e5dcfb9aafc0dc0976f1039d761aea797369128
yousefsayed <yousef16992@gmail.com>
2026-04-16 20:42:44 +0200

    add lab2 and modified lab1

--- src/lab1/Calculator.java
+++ src/lab1/Calculator.java
@@ -3,5 +3,8 @@ package lab1;
 public class Calculator {
     public static void main(String[] args){
         System.out.println("test");
+        int a = 5;
+        int b = 3;
+        System.out.println("Sum: " + (a + b));
     }
 }
---
+++ src/lab2/BankService.java
@@ -0,0 +1,7 @@
+package lab2;
+
+public class BankService {
+    public static void main(String[] args) {
+        System.out.println("bank service");
+    }
+}
