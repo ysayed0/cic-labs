66b504bb7bf5429c0589f80b292cae4f6fdeba09
yousefsayed <yousef16992@gmail.com>
2026-04-16 20:54:33 +0200

    test

--- src/lab1/Calculator.java
+++ src/lab1/Calculator.java
@@ -3,8 +3,5 @@ package lab1;
 public class Calculator {
     public static void main(String[] args){
         System.out.println("test");
-        int a = 5;
-        int b = 3;
-        System.out.println("Sum: " + (a + b));
     }
 }
